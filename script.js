8566aa43cafd9edc7a46a571e44aaa191601796f
kaualima4467 <kaua.oliveira01@escola.pr.gov.br>
2025-09-16 16:58:24 -0300

    dia 16 aula de progamaçao

--- script.js
+++ script.js
@@ -9,3 +9,53 @@ function mostrapalavrachave(){
     campoResultado.textContent = palavras.join(",");
     
 }
+
+function processatexto(texto) {
+  let palavras = texto.splint(/\P{L}+/u);
+
+const frequencias = contafrequencias(palavras)
+let ordenadas = Object.keys(frequencias).sort(ordenapalavra);
+
+function ordenapalavra(p1,p2){
+  returnfrequencias [p2]-frequencias[p1];
+
+
+
+}
+console.log(ordenadas);
+    return ordenadas.slice(0,10);
+
+
+
+
+}
+
+
+
+function contafrequencias(palavras){
+let frequencias = {};
+for (let i of palavras){
+    frequencias[i] = 0;
+for(let j of palavras){
+if (i==j){
+
+frequencias[i]
+}
+
+}
+
+
+}
+
+
+
+
+
+
+}
+
+return palavras;
+
+
+
+}
